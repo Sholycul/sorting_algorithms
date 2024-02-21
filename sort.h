3cd8a0d5818ecec9d771393a9876ce5840801d30
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-02-21 01:36:50 -0800

    Added quick sort

--- sort.h
+++ sort.h
@@ -54,6 +54,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 listint_t *swap_node(listint_t *node, listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 
